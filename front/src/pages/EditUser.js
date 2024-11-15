2459b1b33d80a0abae4403faab8a9247489a537d
ChiaYi <146308280+b11007037@users.noreply.github.com.>
2024-11-15 15:49:13 +0800

    init front

---
+++ front/src/pages/EditUser.js
@@ -0,0 +1,2 @@
+const EditUser = () => {};
+export default EditUser;
---
+++ front/src/pages/Home.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+    const [users, setUsers] = useState();
+
+    useEffect(()=>{
+        const fetchUsers = async () => {
+            const res = await fetch(`${process.env/REACT_APP_SERVER_URL}/user`);
+            const data = await res.json();
+            setUsers(data);
+        }; //宣告
+        fetchUsers(data); //呼叫
+    },[]);
+
+    const handleDelete = async (id) => {};
+
+    return (
+        <div>
+            {users?.map( (user) => (
+                <div>
+                    <img src={user.avater} width={"100%"} height={200}/>
+                    <div>
+                      <h4>{user.name}</h4>
+                      <div>
+                          <Link to={`/edit/${user.id}`}>Edit</Link>
+                          <button onClick={() => { handleDelete(user.id) }}></button>
+                      </div>
+                    </div>
+                </div>
+            ) )};
+        </div>
+    );
+};
+
+export default Home;
